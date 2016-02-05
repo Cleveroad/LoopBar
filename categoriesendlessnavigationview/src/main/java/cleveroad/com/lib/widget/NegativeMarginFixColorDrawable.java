16f5e85447d3c9f96874d3f752ed6bdff1f9d32e
Beloo <BeloyOleg@yandex.ru>
2016-02-05 18:08:13 +0200

    * negativeMargin colorDrawable updated

--- categoriesendlessnavigationview/src/main/java/cleveroad/com/lib/widget/NegativeMarginFixColorDrawable.java
+++ categoriesendlessnavigationview/src/main/java/cleveroad/com/lib/widget/NegativeMarginFixColorDrawable.java
@@ -1,6 +1,7 @@
 package cleveroad.com.lib.widget;
 
 import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.graphics.Region;
 
 public class NegativeMarginFixColorDrawable extends android.graphics.drawable.ColorDrawable {
@@ -11,6 +12,9 @@ public class NegativeMarginFixColorDrawable extends android.graphics.drawable.Co
 
     @Override
     public void draw(Canvas canvas) {
+//        Rect bounds = new Rect(getBounds());
+//        bounds.top = bounds.top - 10;
+
         canvas.clipRect(getBounds(), Region.Op.REPLACE);
         super.draw(canvas);
     }
--- sample/src/main/java/cleveroad/com/endlessnavigationview/MainActivity.java
+++ sample/src/main/java/cleveroad/com/endlessnavigationview/MainActivity.java
@@ -1,9 +1,16 @@
 package cleveroad.com.endlessnavigationview;
 
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import cleveroad.com.lib.adapter.ICategoryItem;
 import cleveroad.com.lib.adapter.IOperationItem;
@@ -14,19 +21,62 @@ import cleveroad.com.lib.widget.OnItemClickListener;
 
 public class MainActivity extends AppCompatActivity implements OnItemClickListener<ICategoryItem>{
 
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
         EndlessNavigationView endlessNavigationView = (EndlessNavigationView) findViewById(R.id.endlessView);
 
-        SimpleCategoriesAdapter adapter = new SimpleCategoriesAdapter(MockedItemsFactory.getCategoryItems());
+        List<ICategoryItem> items = MockedItemsFactory.getCategoryItems();
+
+        SimpleCategoriesAdapter adapter = new SimpleCategoriesAdapter(items);
         adapter.addOnItemClickListener(this);
         endlessNavigationView.setCategoriesAdapter(adapter);
+
+        viewPager.setAdapter(new ViewPagerAdapter(items));
+    }
+
+    public static class ViewPagerAdapter extends PagerAdapter {
+        private List<ICategoryItem> items;
+
+        public ViewPagerAdapter(List<ICategoryItem> items){
+            this.items = items;
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            TextView itemView = new TextView(container.getContext());
+            ViewPager.LayoutParams params = new ViewPager.LayoutParams();
+            params.width = ViewPager.LayoutParams.MATCH_PARENT;
+            params.height = ViewPager.LayoutParams.MATCH_PARENT;
+            itemView.setLayoutParams(params);
+            itemView.setGravity(Gravity.CENTER);
+            itemView.setText(items.get(position).toString());
+            container.addView(itemView);
+            return itemView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
     }
 
     @Override
     public void onItemClicked(ICategoryItem item, int position) {
-
+        viewPager.setCurrentItem(position);
     }
 }
