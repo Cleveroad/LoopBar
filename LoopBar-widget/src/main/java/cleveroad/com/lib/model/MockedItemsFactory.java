fd36f42794dd85e1b3e6627980450c71f62d4a95
Beloo <Awful.Vi@yandex.ru>
2016-05-23 13:11:53 +0300

    * change sample title

--- LoopBar-widget/src/main/java/cleveroad/com/lib/model/MockedItemsFactory.java
+++ LoopBar-widget/src/main/java/cleveroad/com/lib/model/MockedItemsFactory.java
@@ -23,11 +23,11 @@ public class MockedItemsFactory {
         items.add(new CategoryItem(R.drawable.enls_ic_local_taxi, "auto"));
         items.add(new CategoryItem(R.drawable.enls_ic_account_balance, "balance"));
         items.add(new CategoryItem(R.drawable.enls_ic_alarm, "alarm"));
-        items.add(new CategoryItem(android.R.drawable.ic_delete, "cross"));
-        items.add(new CategoryItem(android.R.drawable.ic_menu_camera, "photo"));
-        items.add(new CategoryItem(android.R.drawable.ic_search_category_default, "search"));
-        items.add(new CategoryItem(android.R.drawable.ic_dialog_alert, "alert"));
-        items.add(new CategoryItem(android.R.drawable.ic_dialog_email, "email"));
+        items.add(new CategoryItem(R.drawable.enls_vector_brush_white_24dp, "brush"));
+        items.add(new CategoryItem(R.drawable.enls_vector_camera_alt_white_24dp, "camera"));
+        items.add(new CategoryItem(R.drawable.enls_vector_landscape_white_24dp, "rock"));
+        items.add(new CategoryItem(R.drawable.enls_vector_palette_white_24dp, "palette"));
+        items.add(new CategoryItem(R.drawable.enls_vector_moon_white_24dp, "moon"));
         return items;
     }
 }
