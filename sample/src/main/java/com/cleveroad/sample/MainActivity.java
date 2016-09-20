ea566177143abbc6b656f7bbe488933c79fe0691
Peter <illechenko.cr@gmail.com>
2016-09-20 19:11:46 +0300

    feat: samples refactor: deleted unused code

--- sample/src/main/java/com/cleveroad/sample/MainActivity.java
+++ sample/src/main/java/com/cleveroad/sample/MainActivity.java
@@ -1,99 +1,33 @@
 package com.cleveroad.sample;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
 
-import com.cleveroad.loopbar.model.MockedItemsFactory;
-import com.cleveroad.loopbar.widget.LoopBarView;
-import com.cleveroad.loopbar.widget.OnItemClickListener;
 import com.cleveroad.loopbar.widget.Orientation;
+import com.cleveroad.sample.fragments.CategoriesAdapterLoopBarFragment;
+import com.cleveroad.sample.fragments.MenuLoopBarFragment;
+import com.cleveroad.sample.fragments.ViewPagerLoopBarFragment;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class MainActivity extends AppCompatActivity implements OnItemClickListener {
-    private static final String EXTRA_ORIENTATION = "orientation";
-
-    private LoopBarView loopBarView;
-    private SimpleFragmentStatePagerAdapter pagerAdapter;
-    private ViewPager viewPager;
-
-    //args
-    @Orientation
-    private int orientation;
-    @LoopBarView.GravityAttr
-    private int endlessGravity = LoopBarView.SELECTION_GRAVITY_START;
-
-    public static void start(Context context, int orientation) {
-        Intent starter = new Intent(context, MainActivity.class);
-        starter.putExtra(EXTRA_ORIENTATION, orientation);
-        context.startActivity(starter);
-    }
-
-    public void onBtnOrientationClicked(View btn) {
-        int nextOrientation = orientation == Orientation.ORIENTATION_VERTICAL ? Orientation.ORIENTATION_HORIZONTAL : Orientation.ORIENTATION_VERTICAL;
-        start(this, nextOrientation);
-        finish();
-    }
-
-    public void onBtnGravityClicked(View btn) {
-        int nextGravity = endlessGravity == LoopBarView.SELECTION_GRAVITY_START ?
-                LoopBarView.SELECTION_GRAVITY_END : LoopBarView.SELECTION_GRAVITY_START;
-        endlessGravity = nextGravity;
-        loopBarView.setGravity(nextGravity);
-    }
+public class MainActivity extends AppCompatActivity implements IFragmentReplacer {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        @Orientation
-        int orientation = getIntent().getIntExtra(EXTRA_ORIENTATION, Orientation.ORIENTATION_HORIZONTAL);
-        this.orientation = orientation;
-
-        setContentView(orientation == Orientation.ORIENTATION_VERTICAL ? R.layout.activity_main_vertical : R.layout.activity_main_horizontal);
-
-        loopBarView = (LoopBarView) findViewById(R.id.endlessView);
-        viewPager = (ViewPager) findViewById(R.id.viewPager);
-
-        loopBarView.addOnItemClickListener(this);
-
-        List<Fragment> list = new ArrayList<>(8);
-        list.add(ColorFragment.newInstance(android.R.color.holo_red_dark));
-        list.add(ColorFragment.newInstance(android.R.color.black));
-        list.add(ColorFragment.newInstance(android.R.color.holo_green_dark));
-        list.add(ColorFragment.newInstance(android.R.color.holo_orange_dark));
-        list.add(ColorFragment.newInstance(android.R.color.holo_blue_light));
-        list.add(ColorFragment.newInstance(android.R.color.holo_green_light));
-        list.add(ColorFragment.newInstance(android.R.color.holo_purple));
-        list.add(ColorFragment.newInstance(android.R.color.white));
-
-        pagerAdapter = new SimpleFragmentStatePagerAdapter(getSupportFragmentManager(), list, MockedItemsFactory.getCategoryItems(this));
-        viewPager.setAdapter(pagerAdapter);
-        viewPager.addOnPageChangeListener(new AbstractPageChangedListener() {
-            @Override
-            public void onPageSelected(int position) {
-                super.onPageSelected(position);
-                loopBarView.setCurrentItem(position);
-            }
-
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                Log.d("tag", "on page scrolled");
-            }
-        });
-
-        loopBarView.setupWithViewPager(viewPager);
+        if (savedInstanceState == null) {
+            replaceFragment(CategoriesAdapterLoopBarFragment.newInstance(Orientation.ORIENTATION_VERTICAL));
+//            replaceFragment(MenuLoopBarFragment.newInstance(Orientation.ORIENTATION_VERTICAL));
+//            replaceFragment(ViewPagerLoopBarFragment.newInstance(Orientation.ORIENTATION_VERTICAL));
+        }
     }
 
     @Override
-    public void onItemClicked(int position) {
-        viewPager.setCurrentItem(position);
+    public void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
     }
 }
